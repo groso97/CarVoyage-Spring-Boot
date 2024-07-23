231ea3bd78aed3d178f454d7b2275f5a437afcc6
Goran Roso <fanatic.roso@gmail.com>
2024-07-23 13:32:55 +0200

    Na pocetnoj dodana faq sekcija

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -2,23 +2,47 @@ const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
 const carouselInner = document.querySelector(".carousel-inner");
 let index = 0;
+const itemsPerPage = 3;
 
 function updateCarousel() {
   const totalItems = document.querySelectorAll(".carousel-item").length;
-  const newIndex = (index + totalItems) % totalItems;
-  carouselInner.style.transform = `translateX(-${newIndex * 100}%)`;
-  index = newIndex;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  
+  if (index <= 0) {
+    index = 0;
+    prevBtn.disabled = true;
+  } else {
+    prevBtn.disabled = false;
+  }
+  
+  if (index >= totalPages - 1) {
+    index = totalPages - 1;
+    nextBtn.disabled = true;
+  } else {
+    nextBtn.disabled = false;
+  }
+
+  carouselInner.style.transform = `translateX(-${index * 100}%)`;
 }
 
 prevBtn.addEventListener("click", () => {
-  index--;
-  updateCarousel();
+  if (index > 0) {
+    index--;
+    updateCarousel();
+  }
 });
 
 nextBtn.addEventListener("click", () => {
-  index++;
-  updateCarousel();
+  const totalItems = document.querySelectorAll(".carousel-item").length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  if (index < totalPages - 1) {
+    index++;
+    updateCarousel();
+  }
 });
+
+updateCarousel(); // Initial call to set the correct state of the buttons
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -37,3 +61,32 @@ document.addEventListener("DOMContentLoaded", function () {
     // Onemogući izbor prošlih datuma i vremena
     dateTimeInput.min = currentDateTime;
   });
+
+
+
+  function showCategory(category) {
+    const categories = document.querySelectorAll('.faq-category');
+    categories.forEach(cat => cat.classList.add('hidden'));
+
+    document.getElementById(category).classList.remove('hidden');
+
+    const buttons = document.querySelectorAll('button[id$="-btn"]');
+    buttons.forEach(btn => btn.classList.remove('bg-[#1ECB15]', 'text-white'));
+    buttons.forEach(btn => btn.classList.add('bg-gray-100', 'text-gray-700'));
+
+    document.getElementById(category + '-btn').classList.remove('bg-gray-100', 'text-gray-700');
+    document.getElementById(category + '-btn').classList.add('bg-[#1ECB15]', 'text-white');
+}
+
+function toggleAnswer(index) {
+    const answers = document.querySelectorAll('.faq-answer');
+    
+    // Hide all answers
+    answers.forEach(answer => answer.classList.add('hidden'));
+
+    // Show the selected answer
+    answers[index].classList.remove('hidden');
+}
+
+// Default view
+showCategory('reservation');
